7d2e21a95bae2729d72b9ce13f997257af9b203a
Simon <lou.simon.2287@gmail.com>
2025-11-20 11:58:39 +0100

    ajout ractions

--- dashboard/models/__init__.py
+++ dashboard/models/__init__.py
@@ -9,6 +9,7 @@ from .models import (
     Course, 
     Event,
     ForumAttachment,
+    ForumReaction,
 )
 
 # Importe le module de signaux pour s'assurer que les récepteurs sont chargés au démarrage de l'application.
